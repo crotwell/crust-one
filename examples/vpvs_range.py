bae6fa3c4883ba8ce576f8dd4395a506a5a3fa43
Philip Crotwell <crotwell@seis.sc.edu>
2022-02-18 16:20:49 -0500

    format floats

--- examples/vpvs_range.py
+++ examples/vpvs_range.py
@@ -7,12 +7,12 @@ min=9999
 max=-1
 for p in c1.profiles.values():
     vpvs = p.avg_vpvs()
-    #print(f"{p.lat},{p.lon},{vpvs}")
+    #print(f"{vpvs:3.2f},{p.lat},{p.lon}")
     if vpvs > max:
         max = vpvs
     if vpvs < min:
         min = vpvs
-print(f"Crust1.0 vp/vs Min: {min},  Max: {max}")
+print(f"Crust1.0 vp/vs Min: {min:3.2f},  Max: {max:3.2f}")
 
 
 c2 = crustone.CrustTwo()
@@ -22,9 +22,9 @@ max=-1
 for p in c2.profiles.values():
     vpvs = p.asProfile().avg_vpvs()
     no_comma = p.name.replace(',','\,')
-    #print(f"{p.code},{no_comma},{vpvs}")
+    #print(f"{vpvs:3.2f},{p.code},{no_comma}")
     if vpvs > max:
         max = vpvs
     if vpvs < min:
         min = vpvs
-print(f"Crust2.0 vp/vs Min: {min},  Max: {max}")
+print(f"Crust2.0 vp/vs Min: {min:3.2f},  Max: {max:3.2f}")
